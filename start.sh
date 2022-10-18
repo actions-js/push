7d5ceaa1951e4e032a158570f7f0725a3997dad9
Enes <66560757+devenes@users.noreply.github.com>
2022-10-18 21:13:45 +0300

    feat: add support for amend input
    Signed-off-by: Enes <66560757+devenes@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ INPUT_COAUTHOR_EMAIL=${INPUT_COAUTHOR_EMAIL:-''}
 INPUT_COAUTHOR_NAME=${INPUT_COAUTHOR_NAME:-''}
 INPUT_MESSAGE=${INPUT_MESSAGE:-"chore: autopublish ${timestamp}"}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_AMEND=${INPUT_AMEND:-false}
 INPUT_FORCE=${INPUT_FORCE:-false}
 INPUT_TAGS=${INPUT_TAGS:-false}
 INPUT_EMPTY=${INPUT_EMPTY:-false}
@@ -23,6 +24,10 @@ echo "Push to branch $INPUT_BRANCH";
 
 if ${INPUT_EMPTY}; then
     _EMPTY='--allow-empty'
+fi
+
+if ${INPUT_AMEND}; then
+    _AMEND='--amend --no-edit'
 fi
 
 if ${INPUT_FORCE}; then
@@ -43,7 +48,16 @@ git config --local user.name "${INPUT_AUTHOR_NAME}"
 
 git add -A
 
-if [ -n "${INPUT_COAUTHOR_EMAIL}" ] && [ -n "${INPUT_COAUTHOR_NAME}" ]; then
+if ${INPUT_AMEND}; then
+    if [ -n "${INPUT_COAUTHOR_EMAIL}" ] && [ -n "${INPUT_COAUTHOR_NAME}" ]; then
+        git commit ${_AMEND} -m "${INPUT_MESSAGE}
+
+    Co-authored-by: ${INPUT_COAUTHOR_NAME} <${INPUT_COAUTHOR_EMAIL}>" || exit 0
+    else
+    git commit ${_AMEND} -m "${INPUT_MESSAGE}" $_EMPTY || exit 0
+    fi
+
+elif [ -n "${INPUT_COAUTHOR_EMAIL}" ] && [ -n "${INPUT_COAUTHOR_NAME}" ]; then
     git commit -m "${INPUT_MESSAGE}
     
 
